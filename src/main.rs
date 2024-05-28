569eb84be032ef90b0da271b9765075d4722f00d
Peter Hutterer <peter.hutterer@who-t.net>
2024-05-29 09:49:19 +1000

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,32 @@ fn send_usb_request(device: &Device) {
     let handle = device.open().unwrap();
     // See the uclogic driver
     const MAGIC_LANGUAGE_ID: u16 = 0x409;
-    handle.read_languages(timeout).unwrap().iter().filter(|l| l.lang_id() == MAGIC_LANGUAGE_ID).for_each(|lang| {
-        // Firmware call for Huion devices
-        let s = handle.read_string_descriptor(*lang, 201, timeout).unwrap();
-        println!("HUION_FIRMWARE_ID={s}");
-        // Get the pen input parameters, see uclogic_params_pen_init_v2()
-        // This retrieves magic configuratino parameters but more importantly
-        // switches the tablet to send events on the 0x8 Report ID (88 bits of Vendor Usage in
-        // Usage Page 0x00FF).
-        let s = handle.read_string_descriptor(*lang, 200, timeout).unwrap();
-        if s.as_bytes().len() >= 18 {
-            let bytes: Vec<String> = s.as_bytes().iter().map(|b| format!("{b:02x}")).collect();
-            println!("HUION_MAGIC_BYTES={}", bytes.join(""));
-        } else {
-            let s = handle.read_string_descriptor(*lang, 100, timeout).unwrap();
-            let bytes: Vec<String> = s.as_bytes().iter().map(|b| format!("{b:02x}")).collect();
-            println!("HUION_MAGIC_BYTES={}", bytes.join(""));
-            // switch the buttons into raw mode
-            let s = handle.read_string_descriptor(*lang, 123, timeout).unwrap();
-            println!("HUION_PAD_MODE={s}");
-        }
-    });
+    handle
+        .read_languages(timeout)
+        .unwrap()
+        .iter()
+        .filter(|l| l.lang_id() == MAGIC_LANGUAGE_ID)
+        .for_each(|lang| {
+            // Firmware call for Huion devices
+            let s = handle.read_string_descriptor(*lang, 201, timeout).unwrap();
+            println!("HUION_FIRMWARE_ID={s}");
+            // Get the pen input parameters, see uclogic_params_pen_init_v2()
+            // This retrieves magic configuratino parameters but more importantly
+            // switches the tablet to send events on the 0x8 Report ID (88 bits of Vendor Usage in
+            // Usage Page 0x00FF).
+            let s = handle.read_string_descriptor(*lang, 200, timeout).unwrap();
+            if s.as_bytes().len() >= 18 {
+                let bytes: Vec<String> = s.as_bytes().iter().map(|b| format!("{b:02x}")).collect();
+                println!("HUION_MAGIC_BYTES={}", bytes.join(""));
+            } else {
+                let s = handle.read_string_descriptor(*lang, 100, timeout).unwrap();
+                let bytes: Vec<String> = s.as_bytes().iter().map(|b| format!("{b:02x}")).collect();
+                println!("HUION_MAGIC_BYTES={}", bytes.join(""));
+                // switch the buttons into raw mode
+                let s = handle.read_string_descriptor(*lang, 123, timeout).unwrap();
+                println!("HUION_PAD_MODE={s}");
+            }
+        });
 }
 
 fn main() {
@@ -43,7 +48,7 @@ fn main() {
                 false
             }
         })
-        .for_each(|d|  {
+        .for_each(|d| {
             send_usb_request(&d);
         });
 }
