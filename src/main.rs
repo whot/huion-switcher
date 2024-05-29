8b75cacbcb8b78f62b1473c4c26bdb583e2df3fb
Peter Hutterer <peter.hutterer@who-t.net>
2024-05-29 11:44:35 +1000

    Re-export parent properties if called with a syspath
    If we call this tool with a syspath argument, run up the udev
    device parents to search for the various properties we set. If we find
    any, re-export them.
    
    In the udev rule we can then call this tool for each matching syspath
    and will just copy the udev property up instead of re-running the usb
    command.
    
    Finally, in the udev rule we can copy the value over to UNIQ and
    HID_UNIQ depending on the device we have.
    
    No, for some reason IMPORT{parent} did not work for the HUION properties
    we're adding...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use anyhow::Result;
 use libusb::*;
+use std::path::PathBuf;
 
-fn send_usb_request(device: &Device) {
+fn send_usb_request(device: &Device) -> Result<()> {
     let timeout = std::time::Duration::from_millis(100);
-    let handle = device.open().unwrap();
+    let handle = device.open()?;
     // See the uclogic driver
     const MAGIC_LANGUAGE_ID: u16 = 0x409;
     handle
@@ -31,24 +33,66 @@ fn send_usb_request(device: &Device) {
                 println!("HUION_PAD_MODE={s}");
             }
         });
+
+    Ok(())
 }
 
-fn main() {
+fn send_usb() -> Result<()> {
     let ctx = Context::new().unwrap();
 
     const HUION_VENDOR_ID: u16 = 0x256C;
 
-    ctx.devices()
-        .unwrap()
-        .iter()
-        .filter(|d| {
-            if let Ok(desc) = d.device_descriptor() {
-                desc.vendor_id() == HUION_VENDOR_ID
-            } else {
-                false
+    for device in ctx.devices().unwrap().iter().filter(|d| {
+        if let Ok(desc) = d.device_descriptor() {
+            desc.vendor_id() == HUION_VENDOR_ID
+        } else {
+            false
+        }
+    }) {
+        send_usb_request(&device)?;
+    }
+
+    Ok(())
+}
+
+fn search_udev(path: &str) -> Result<()> {
+    let path = PathBuf::from(path);
+    let mut device = udev::Device::from_syspath(&path)?;
+    let properties: Vec<&str> = vec!["HUION_FIRMWARE_ID", "HUION_MAGIC_BYTES", "HUION_PAD_MODE"];
+
+    loop {
+        // We expect all properties to be set on the same device
+        if properties.iter().any(|p| {
+            let v = device.property_value(p);
+            if let Some(v) = v {
+                println!("{p}={}", v.to_string_lossy());
             }
-        })
-        .for_each(|d| {
-            send_usb_request(&d);
-        });
+            v.is_some()
+        }) {
+            break;
+        }
+
+        if let Some(parent) = device.parent() {
+            device = parent;
+        } else {
+            // we're out of udev parents so no device has the
+            // property set yet. Which means we should send the USB request.
+            send_usb()?;
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let strs: Vec<&str> = args.iter().map(|x| x.as_str()).collect();
+    let rc = match &strs[..] {
+        [path] => search_udev(path),
+        _ => send_usb(),
+    };
+    if let Err(e) = rc {
+        eprintln!("Error: {e}");
+    }
 }
